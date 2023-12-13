fa31c025a2ab598cd01a7212f6593adc02faa237
Sumit Das Mohapatra <sumitdasmohapatra5@gmail.com>
2023-12-13 20:47:20 +0530

    final edit

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$con = mysqli_connect("localhost","id14603375_root","Ordermania1234@","id14603375_mycart");
-	if (mysqli_connect_errno()){
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		die();
-		}
-?>
